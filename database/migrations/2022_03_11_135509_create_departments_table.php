75c581dd939419f29edc09f44c40574b3d678a80
mais <mais.mh2000@gmail.com>
2022-04-02 06:50:01 +0300

    update migrate

--- database/migrations/2022_03_11_135509_create_departments_table.php
+++ database/migrations/2022_03_11_135509_create_departments_table.php
@@ -11,7 +11,7 @@ class CreateDepartmentsTable extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
             $table->softDeletes();
         });
